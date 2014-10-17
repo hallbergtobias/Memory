bdf3bed8be460b8a4b4a7fe2799d069648cc8088
toobew <toobew_@hotmail.com>
2014-10-17 16:45:29 +0200

    testar

---
+++ Kort.java
@@ -0,0 +1,20 @@
+/**
+ * @author Tobias Hallberg
+ */
+public class Kort extends JColorfulButton {
+
+    public enum Status {
+        DOLT, SYNLIGT, SAKNAS
+    }
+
+
+
+    private class Icon { //bild för varje kort. Osäker på den ska vara en inner class eller inte
+        //Om DOLT => blå, SAKNAR => vit, SYNLIGT=> bild
+
+
+
+
+    }
+
+}
